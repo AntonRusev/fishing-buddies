e900303a3e50fa632958591d56d86b2166c6a29d
AntonRusev <anton_r@abv.bg>
2023-12-09 16:17:12 +0200

    implemented exceptions handling middleware

--- API/Program.cs
+++ API/Program.cs
@@ -1,4 +1,5 @@
 using API.Extensions;
+using API.Middleware;
 using Microsoft.EntityFrameworkCore;
 using Persistence;
 
@@ -11,12 +12,15 @@ builder.Services.AddApplicationServices(builder.Configuration);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseMiddleware<ExceptionMiddleware>();
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+// app.UseCors("CorsPolicy");
 // app.UseAuthentication();
 
 app.MapControllers();
