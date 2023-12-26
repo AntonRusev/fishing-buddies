7394575301cc0c5a5b2be2876e795d9b46424af8
AntonRusev <anton_r@abv.bg>
2023-12-26 11:20:40 +0200

    added Delete Photo and Set Main Photo functionality

--- API/Controllers/PhotosController.cs
+++ API/Controllers/PhotosController.cs
@@ -5,10 +5,25 @@ namespace API.Controllers
 {
     public class PhotosController : BaseApiController
     {
+        // Adding a Photo
         [HttpPost]
         public async Task<IActionResult> AddPhoto([FromForm] AddPhoto.Command command)
         {
             return HandleResult(await Mediator.Send(command));
+        }
+
+        // Deleting a Photo
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePhoto(string id)
+        {
+            return HandleResult(await Mediator.Send(new DeletePhoto.Command { Id = id }));
+        }
+
+        // Setting a photo to Main Photo
+        [HttpPost("{id}/setMain")]
+        public async Task<IActionResult> SetMain(string id)
+        {
+            return HandleResult(await Mediator.Send(new SetMain.Command { Id = id }));
         }
     }
 }
